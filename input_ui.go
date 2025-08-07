1b857959b4428c526df1602cea70ff111b289cfc
Distortions81 <carlotto81@gmail.com>
2025-08-07 02:10:23 -0600

    .

--- input_ui.go
+++ input_ui.go
@@ -6,6 +6,9 @@ import "github.com/Distortions81/EUI/eui"
 func pointInUI(x, y int) bool {
 	fx, fy := float32(x), float32(y)
 	for _, win := range eui.Windows() {
+		if !win.Open {
+			continue
+		}
 		pos := win.GetPos()
 		size := win.GetSize()
 		if fx >= pos.X && fx < pos.X+size.X && fy >= pos.Y && fy < pos.Y+size.Y {
@@ -13,6 +16,9 @@ func pointInUI(x, y int) bool {
 		}
 	}
 	for _, ov := range eui.Overlays() {
+		if !ov.Open {
+			continue
+		}
 		pos := ov.GetPos()
 		size := ov.GetSize()
 		if fx >= pos.X && fx < pos.X+size.X && fy >= pos.Y && fy < pos.Y+size.Y {
