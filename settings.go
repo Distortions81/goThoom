e5a6708e41219d03bc03d25255b73c3a9f097d35
Carl Otto <33408758+Distortions81@users.noreply.github.com>
2025-08-08 00:49:08 -0600

    Fix mobile name background alpha and set default opacity

--- settings.go
+++ settings.go
@@ -17,7 +17,7 @@ var gs settings = settings{
 	MainFontSize:   8,
 	BubbleFontSize: 6,
 	BubbleOpacity:  160.0 / 255.0,
-	NameBgOpacity:  1.0,
+	NameBgOpacity:  0.66,
 
 	NightEffect:      true,
 	SpeechBubbles:    true,
