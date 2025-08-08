e0c8865032f35d4a31ed355d065ab38aa52700e9
Carl Otto <33408758+Distortions81@users.noreply.github.com>
2025-08-07 23:15:14 -0600

    Exclude NPCs from players list

--- players_ui.go
+++ players_ui.go
@@ -20,34 +20,20 @@ func updatePlayersWindow() {
 	sort.Slice(ps, func(i, j int) bool { return ps[i].Name < ps[j].Name })
 	playersList.Contents = playersList.Contents[:0]
 
-	var exiles, npcs []Player
+	var exiles []Player
 	for _, p := range ps {
-		if p.Name == "" {
+		if p.Name == "" || p.IsNPC {
 			continue
 		}
-		if p.IsNPC {
-			npcs = append(npcs, p)
-		} else {
-			exiles = append(exiles, p)
-		}
+		exiles = append(exiles, p)
 	}
 
 	buf := fmt.Sprintf("Players Online: %v", len(exiles))
 	t, _ := eui.NewText(&eui.ItemData{ItemType: eui.ITEM_TEXT, Text: buf, FontSize: 10, Size: eui.Point{X: 100, Y: 24}})
 	playersList.AddItem(t)
 	for _, p := range exiles {
-		t, _ := eui.NewText(&eui.ItemData{Text: p.Name, FontSize: 10, Size: eui.Point{X: 100, Y: 24}})
+		t, _ = eui.NewText(&eui.ItemData{Text: p.Name, FontSize: 10, Size: eui.Point{X: 100, Y: 24}})
 		playersList.AddItem(t)
-	}
-
-	if len(npcs) > 0 {
-		buf = fmt.Sprintf("NPCs: %v", len(npcs))
-		t, _ = eui.NewText(&eui.ItemData{ItemType: eui.ITEM_TEXT, Text: buf, FontSize: 10, Size: eui.Point{X: 100, Y: 24}})
-		playersList.AddItem(t)
-		for _, p := range npcs {
-			t, _ := eui.NewText(&eui.ItemData{Text: p.Name, FontSize: 10, Size: eui.Point{X: 100, Y: 24}})
-			playersList.AddItem(t)
-		}
 	}
 	if playersWin != nil {
 		playersWin.Refresh()
