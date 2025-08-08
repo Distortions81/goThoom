e172641a7b80ef98fcc3d5ba798a31b47ad39bdc
Carl Otto <33408758+Distortions81@users.noreply.github.com>
2025-08-08 16:30:32 -0600

    Add movie recording writer

---
+++ movie_recorder.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"encoding/binary"
+	"os"
+	"time"
+)
+
+type fileHead struct {
+	Signature    uint32
+	Version      uint16
+	Len          uint16
+	Frames       int32
+	StartTime    uint32
+	Revision     int32
+	OldestReader int32
+}
+
+type frameHead struct {
+	Signature uint32
+	Frame     int32
+	Size      uint16
+	Flags     uint16
+}
+
+type movieRecorder struct {
+	f    *os.File
+	head fileHead
+}
+
+const macEpochDelta = 2082844800
+
+func newMovieRecorder(path string, version, revision int) (*movieRecorder, error) {
+	f, err := os.Create(path)
+	if err != nil {
+		return nil, err
+	}
+	mr := &movieRecorder{f: f}
+	mr.head = fileHead{
+		Signature:    movieSignature,
+		Version:      uint16(version),
+		Len:          24,
+		Frames:       0,
+		StartTime:    uint32(time.Now().Unix() + macEpochDelta),
+		Revision:     int32(revision),
+		OldestReader: int32((353 << 8) + 0),
+	}
+	if err := mr.writeHeader(); err != nil {
+		f.Close()
+		return nil, err
+	}
+	return mr, nil
+}
+
+func (m *movieRecorder) writeHeader() error {
+	buf := make([]byte, 24)
+	binary.BigEndian.PutUint32(buf[0:], m.head.Signature)
+	binary.BigEndian.PutUint16(buf[4:], m.head.Version)
+	binary.BigEndian.PutUint16(buf[6:], m.head.Len)
+	binary.BigEndian.PutUint32(buf[8:], uint32(m.head.Frames))
+	binary.BigEndian.PutUint32(buf[12:], m.head.StartTime)
+	binary.BigEndian.PutUint32(buf[16:], uint32(m.head.Revision))
+	binary.BigEndian.PutUint32(buf[20:], uint32(m.head.OldestReader))
+	if _, err := m.f.Seek(0, 0); err != nil {
+		return err
+	}
+	_, err := m.f.Write(buf)
+	return err
+}
+
+func (m *movieRecorder) WriteFrame(data []byte, flags uint16) error {
+	if m.f == nil {
+		return os.ErrClosed
+	}
+	fh := frameHead{
+		Signature: movieSignature,
+		Frame:     m.head.Frames,
+		Size:      uint16(len(data)),
+		Flags:     flags,
+	}
+	m.head.Frames++
+	buf := make([]byte, 12)
+	binary.BigEndian.PutUint32(buf[0:], fh.Signature)
+	binary.BigEndian.PutUint32(buf[4:], uint32(fh.Frame))
+	binary.BigEndian.PutUint16(buf[8:], fh.Size)
+	binary.BigEndian.PutUint16(buf[10:], fh.Flags)
+	if _, err := m.f.Write(buf); err != nil {
+		return err
+	}
+	_, err := m.f.Write(data)
+	return err
+}
+
+func (m *movieRecorder) Close() error {
+	if m.f == nil {
+		return nil
+	}
+	if err := m.writeHeader(); err != nil {
+		m.f.Close()
+		m.f = nil
+		return err
+	}
+	err := m.f.Close()
+	m.f = nil
+	return err
+}
