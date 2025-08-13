5312111fa4045638729f88b97577c15079bba73d
Distortions81 <carlotto81@gmail.com>
2025-08-12 23:25:39 -0600

    .

--- settings.go
+++ settings.go
@@ -40,7 +40,7 @@ var gsdef settings = settings{
 	ShowFPS:           true,
 	UIScale:           1.0,
 	Fullscreen:        false,
-	Volume:            0.5,
+	Volume:            0.25,
 	Mute:              false,
 	GameScale:         2,
 	Theme:             "",
