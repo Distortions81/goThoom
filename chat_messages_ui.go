a9ce803337aa6fc5426ff72b88a9f18a3a09ac7e
Carl Otto <33408758+Distortions81@users.noreply.github.com>
2025-08-13 03:07:21 -0600

    refactor: unify console and chat windows

--- chat_messages_ui.go
+++ chat_messages_ui.go
@@ -2,71 +2,19 @@
 
 package main
 
-import (
-	"go_client/eui"
-)
+import "go_client/eui"
 
 var chatWin *eui.WindowData
 var chatList *eui.ItemData
-var inputBar *eui.ItemData
 
 func updateChatWindow() {
-	if chatList == nil {
-		return
-	}
-	if chatWin != nil {
-		chatList.Size.Y = chatWin.GetSize().Y
-		if inputBar != nil {
-			chatList.Size.Y -= inputBar.Size.Y
-		}
-	}
-	msgs := getChatMessages()
-	changed := false
-	for i, msg := range msgs {
-		if i < len(chatList.Contents) {
-			if chatList.Contents[i].Text != msg || chatList.Contents[i].FontSize != float32(gs.ChatFontSize) {
-				chatList.Contents[i].Text = msg
-				chatList.Contents[i].FontSize = float32(gs.ChatFontSize)
-				changed = true
-			}
-		} else {
-			t, _ := eui.NewText()
-			if t == nil {
-				logError("create chat text: eui.NewText returned nil")
-				continue
-			}
-			t.Text = msg
-			t.FontSize = float32(gs.ChatFontSize)
-			chatList.AddItem(t)
-			changed = true
-		}
-	}
-	if len(chatList.Contents) > len(msgs) {
-		for i := len(msgs); i < len(chatList.Contents); i++ {
-			chatList.Contents[i] = nil
-		}
-		chatList.Contents = chatList.Contents[:len(msgs)]
-		changed = true
-	}
-	if changed && chatWin != nil {
-		chatWin.Refresh()
-	}
+	updateTextWindow(chatWin, chatList, nil, getChatMessages(), gs.ChatFontSize, "")
 }
 
 func makeChatWindow() error {
 	if chatWin != nil {
 		return nil
 	}
-	chatWin = eui.NewWindow()
-	chatWin.Size = eui.Point{X: 410, Y: 450}
-	chatWin.Title = "Chat"
-	chatWin.Closable = true
-	chatWin.Resizable = true
-	chatWin.Movable = true
-	chatWin.SetZone(eui.HZoneRight, eui.VZoneBottom)
-
-	chatList = &eui.ItemData{ItemType: eui.ITEM_FLOW, FlowType: eui.FLOW_VERTICAL, Scrollable: true}
-	chatWin.AddItem(chatList)
-	chatWin.AddWindow(false)
+	chatWin, chatList, _ = makeTextWindow("Chat", eui.HZoneRight, eui.VZoneBottom, false)
 	return nil
 }
--- console_ui.go
+++ console_ui.go
@@ -5,92 +5,21 @@ package main
 import "go_client/eui"
 
 var consoleWin *eui.WindowData
-var consoleFlow *eui.ItemData
 var messagesFlow *eui.ItemData
 var inputFlow *eui.ItemData
-var messagesDirty bool
 
 func updateConsoleWindow() {
-	if messagesFlow == nil || inputFlow == nil {
-		return
-	}
-	msgs := getConsoleMessages()
-	changed := false
-	for i, msg := range msgs {
-		if i < len(messagesFlow.Contents) {
-			if messagesFlow.Contents[i].Text != msg || messagesFlow.Contents[i].FontSize != float32(gs.ConsoleFontSize) {
-				messagesFlow.Contents[i].Text = msg
-				messagesFlow.Contents[i].FontSize = float32(gs.ConsoleFontSize)
-				changed = true
-			}
-		} else {
-			t, _ := eui.NewText()
-			t.Text = msg
-			t.FontSize = float32(gs.ConsoleFontSize)
-			messagesFlow.AddItem(t)
-			changed = true
-		}
-	}
-	if len(messagesFlow.Contents) > len(msgs) {
-		for i := len(msgs); i < len(messagesFlow.Contents); i++ {
-			messagesFlow.Contents[i] = nil
-		}
-		messagesFlow.Contents = messagesFlow.Contents[:len(msgs)]
-		changed = true
-	}
-
-	inputFlow.Size.Y = float32(gs.ConsoleFontSize) + 8
-	if consoleWin != nil {
-		messagesFlow.Size.Y = consoleWin.GetSize().Y - inputFlow.Size.Y
-	}
 	inputMsg := "[Command Input Bar] (Press enter to switch to command mode)"
 	if inputActive {
 		inputMsg = string(inputText)
 	}
-	if len(inputFlow.Contents) == 0 {
-		t, _ := eui.NewText()
-		t.Text = inputMsg
-		t.FontSize = float32(gs.ConsoleFontSize)
-		inputFlow.AddItem(t)
-		changed = true
-	} else {
-		if inputFlow.Contents[0].Text != inputMsg || inputFlow.Contents[0].FontSize != float32(gs.ConsoleFontSize) {
-			inputFlow.Contents[0].Text = inputMsg
-			inputFlow.Contents[0].FontSize = float32(gs.ConsoleFontSize)
-			changed = true
-		}
-	}
-	if changed {
-		messagesDirty = true
-		if consoleWin != nil {
-			consoleWin.Refresh()
-		}
-	}
+	updateTextWindow(consoleWin, messagesFlow, inputFlow, getConsoleMessages(), gs.ConsoleFontSize, inputMsg)
 }
 
 func makeConsoleWindow() {
 	if consoleWin != nil {
 		return
 	}
-	consoleWin = eui.NewWindow()
-	consoleWin.Title = "Console"
-	consoleWin.Size = eui.Point{X: 410, Y: 450}
-	consoleWin.Closable = true
-	consoleWin.Resizable = true
-	consoleWin.Movable = true
-	consoleWin.SetZone(eui.HZoneLeft, eui.VZoneBottom)
-
-	consoleFlow = &eui.ItemData{ItemType: eui.ITEM_FLOW, FlowType: eui.FLOW_VERTICAL, Fixed: true}
-	consoleFlow.Size = consoleWin.GetSize()
-	consoleWin.AddItem(consoleFlow)
-
-	messagesFlow = &eui.ItemData{ItemType: eui.ITEM_FLOW, FlowType: eui.FLOW_VERTICAL, Scrollable: true, Fixed: true}
-	consoleFlow.AddItem(messagesFlow)
-
-	inputFlow = &eui.ItemData{ItemType: eui.ITEM_FLOW, FlowType: eui.FLOW_VERTICAL, Fixed: true}
-	inputFlow.Color = eui.ColorVeryDarkGray
-	consoleFlow.AddItem(inputFlow)
-
-	consoleWin.AddWindow(false)
+	consoleWin, messagesFlow, inputFlow = makeTextWindow("Console", eui.HZoneLeft, eui.VZoneBottom, true)
 	updateConsoleWindow()
 }
