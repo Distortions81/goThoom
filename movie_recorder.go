90d2b42d51dee014bb5b670081e81df6aebd39c6
Carl Otto <33408758+Distortions81@users.noreply.github.com>
2025-09-01 04:31:06 -0600

    Remove duplicate stopRecording

--- movie_recorder.go
+++ movie_recorder.go
@@ -128,17 +128,3 @@ func (m *movieRecorder) Close() error {
 	m.f = nil
 	return err
 }
-
-func stopRecording() {
-	if recorder == nil {
-		return
-	}
-	if err := recorder.Close(); err != nil {
-		logError("stop recording: %v", err)
-	}
-	recorder = nil
-	wroteLoginBlocks = false
-	if recordStatus != nil {
-		recordStatus.Text = ""
-	}
-}
